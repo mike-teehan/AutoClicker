c28431ee91f9cdf63f581adf1fe62f5da69fad95
Mike Teehan <mike.teehan@gmail.com>
2015-06-14 16:31:14 -0400

    Made checkScrollLock() a thing, instead of doing it all inline.

--- autoclicker.sh
+++ autoclicker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function checkScrollLock {
+	SLS=$(xset q | grep LED | awk '{ print $10 }' | sed 's/^0*//')
+	if [ "${SLS}" == "" ]; then
+		SLS=0
+	fi
+	if (((${SLS} & 4) == 4)); then
+		return 0	#true
+	else
+		return 1	#false
+	fi
+}
+
 # hackey for scroll lock
 XMM=$(xmodmap -pm | grep Scroll_Lock | wc -l)
 if [ $XMM -ne 1 ]; then
@@ -11,11 +23,7 @@ XDOINFO=$(xdotool getmouselocation)
 XPOS=$(echo "${XDOINFO}" | awk '{ print $1 }' | cut -b3-)
 YPOS=$(echo "${XDOINFO}" | awk '{ print $2 }' | cut -b3-)
 while true; do
-	SLS=$(xset q | grep LED | awk '{ print $10 }' | sed 's/^0*//')
-	if [ "${SLS}" == "" ]; then
-		SLS=0
-	fi
-	if (((${SLS} & 4) == 4)); then
+	if checkScrollLock; then
 		xdotool mousemove --sync $XPOS $YPOS click 1 mousemove --sync restore
 	fi
 	sleep .1
